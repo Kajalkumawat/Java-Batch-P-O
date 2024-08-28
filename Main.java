fee10882082dc3da483e2e7676cb38d70eda8a97
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-08-28 09:24:50 +0530

    print array through method with array return type

--- Main.java
+++ Main.java
@@ -1,82 +1,25 @@
-// // public class Main {
-// // public static void main(String[] args) {
-// // int b[] = { 78, 45, 34, 78, 35 };
-// // System.out.println(b[2]);
-// // }
-// // }
-// //hetrongenous data stored
-// public class Main {
-// public static void main(String[] args) {
-// Object a[] = { "hello", 8.9f, 'u', 89 };
-// System.out.println(a[3]);
-// // create wrapper clsss
-// Integer b[] = { 4, 5, 6, 8 };
-// // print single element
-// System.out.println(b[2]);
-// }
-// }
-// Array :
-// -----------------------------------------------------------
-// array is used to store multiple value in single variable
-// array always stored homogenous data in single varabile
+public class Main {
+    static int show(int a) {
+        return a;
+    }
 
-// in some exceptions array is used to store heterogenous data in single
-// variable
+    static int[] array1(int arr[]) {
+        return arr;
+    }
 
-// homogenous :int a -->data store number system
+    static void display(int b[]) {
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
 
-// heterogenous :var a -->data store different data :int , float , string etc
-// Object b -->data store different data :int , float , string etc
+    public static void main(String[] args) {
+        System.out.println(show(78));
+        int m = show(78);
+        System.out.println(m);
 
-// array it contain fixed size
-// array memory :contiguous type memory
-
-// array stored index form
-
-// index always start from zero
-// length always start from 1
-
-// length =index+1
-// index = length-1
-// -----------------------------------------------------------
-// contiguous memory :it stored element randomly at any position
-
-// non-contiguous memory :it does not stored element randomly at any position
-// -----------------------------------------------------------
-// how to create array :
-
-// int a[] ={store multiple data };
-
-// two ways :
-// first way : literal | static | constant
-
-// datatype variablename[] ={,,,};
-
-// second way : dynamic
-
-// int a[] =new int[5];
-
-// datatype variable[]=new datatype[size|length|index+1];
-
-// int[] a;
-// int []a;
-// int a[];
-// -----------------------------------------------------------------
-// index : positive : running
-// negative : not running it give compile time error
-// -----------------------------------------------------------------
-// public class Main {
-// public static void main(String[] args) {
-// int a[]={1,2,3,4,5};
-// //consecutive element : series wise
-// int b[]={78,45,34,78,35};
-// }
-// }
-
-// public class Main {
-// public static void main(String[] args) {
-// Object a[] = { "hello", 8.9f, 'u', 89 };
-// System.out.println(a[3]);
-// }
-// }
-// -----------------------------------------------------------------
+        int e[] = { 6, 7, 5, 4, 3, 9 };
+        int h[] = array1(e);
+        display(h);
+    }
+}
