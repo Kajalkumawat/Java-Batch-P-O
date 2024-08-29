19535d25c4e03e63061d01c74d0f8a217980162e
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-08-29 09:15:07 +0530

    basic for loop questions in C programming

--- SumOfArray.java
+++ SumOfArray.java
@@ -1,10 +1,11 @@
 public class SumOfArray {
     public static void main(String[] args) {
         int a[] = { 1, 2, 3, 4, 5 };
-        int sum = 0;
         for (int i = 0; i < a.length; i++) {
-            sum = sum + a[i];
+            if (a[i] % 2 == 0) {
+                System.out.print(a[i] + " ");
+            }
         }
-        System.out.println(sum);
+
     }
 }
