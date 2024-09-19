7dae1194ff172936c3c4d6ee67cd00bcbc883c88
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-19 09:22:21 +0530

    basic sorting problem in java

---
+++ Sorting.java
@@ -0,0 +1,32 @@
+public class Sorting {
+    public static void printArray(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // swap method
+    public static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    // sorting method :
+    public static void sort(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    swap(a, i, j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 1, 4, 5, 2, 3 };
+        sort(a);
+        printArray(a);
+    }
+}
