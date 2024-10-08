e34b510a3d701f33ae023c1c0f3b408594e8fd45
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-08 10:08:33 +0530

    TYPECASTING STRING CONVERT INTO THE INT PRIMITIVE DATA TYPE THROUGH PREDEFINE METHOD INTEGER.PARSEINT

---
+++ StringIntoInt.java
@@ -0,0 +1,12 @@
+public class StringIntoInt {
+    public static void main(String[] args) {
+        String num1 = "12";
+        String num2 = "12";
+        // typecast
+        int a = Integer.parseInt(num1);
+        int b = Integer.parseInt(num2);
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
