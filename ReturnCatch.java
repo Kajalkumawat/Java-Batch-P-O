d5be79f5971302504c2be54e5bc0842c52f132b6
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-09 10:07:43 +0530

    return in catch block

---
+++ ReturnCatch.java
@@ -0,0 +1,16 @@
+public class ReturnCatch {
+    public static void main(String[] args) {
+        try{
+            int a=9/0;
+            System.out.println(a);
+        }
+        catch(Exception ex){
+            System.out.println("handle");
+            return;
+        }
+        finally{
+            System.out.println("finally block");
+        }
+        
+      }
+}
