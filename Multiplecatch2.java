e382ba4835a34b7ea88cf9aedea7a1638203cb1f
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-09 09:30:16 +0530

    EXCEPTION CLASS

---
+++ Multiplecatch2.java
@@ -0,0 +1,14 @@
+public class Multiplecatch2 {
+    public static void main(String[] args) {
+        try{
+            int a=9/0;
+            System.out.println(a);
+        }
+        catch(Exception ex){
+            System.out.println("exception class");
+        }
+        catch(ArithmeticException ex){
+            System.out.println("ArithmeticException");
+        }
+    }
+}
