dde95c8c068da0d18edf2f633389659f52ddd8a7
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-10 10:08:16 +0530

    throw voter id code

---
+++ ThrowNew.java
@@ -0,0 +1,13 @@
+public class ThrowNew extends Exception {
+    public static void voterid(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("you are not valid for vote");
+        } else {
+            System.out.println("you are valid for vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        voterid(3);
+    }
+}
