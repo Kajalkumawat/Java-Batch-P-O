ec1aa7150111a7c9f462cf60a7c56df77b363298
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-10 09:53:27 +0530

    throw case 5

---
+++ ThrowCase5.java
@@ -0,0 +1,7 @@
+public class ThrowCase5 {
+    public static void main(String[] args) {
+        //class used as exception error 
+        throw new ThrowCase5("hello world");
+        //it give compile time errror 
+    }
+}
