43b48444350b8ad0e778f592a8815aff38e31f26
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-10 09:59:23 +0530

    throw case 6

---
+++ ThrowCase6.java
@@ -0,0 +1,9 @@
+public class ThrowCase6 extends RuntimeException {
+    public ThrowCase6(String name) {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        throw new ThrowCase6("hello world");
+    }
+}
