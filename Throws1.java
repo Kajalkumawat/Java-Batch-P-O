80a27c6f2b5549c718036a61451292406b192be3
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-11 09:22:48 +0530

    throws basic

---
+++ Throws1.java
@@ -0,0 +1,8 @@
+public class Throws1{
+   static void show() throws ArithmeticException{
+        System.out.print("Handle");
+    }
+    public static void main(String[] args) {
+       show(); 
+    }
+}
