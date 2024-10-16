a3f7ad15a3a2a5ab48903c2ac420bcda5a14c818
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-16 09:24:48 +0530

    YIELD METHOD IN MULTITHREADING

---
+++ MethodsInMultithreading1.java
@@ -0,0 +1,18 @@
+class A extends Thread {
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+            Thread.yield();
+            System.out.println("parent child");
+        }
+    }
+}
+
+public class MethodsInMultithreading1 {
+    public static void main(String[] args) {
+        A k = new A();
+        k.start();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("main thread");
+        }
+    }
+}
