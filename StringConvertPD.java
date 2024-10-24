cc5b903078934d64193d90950fb58871979d9583
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-10-24 11:00:32 +0530

    STRING CONVERT INTO PRIMITIVE DATA TYPE IN JAVA

---
+++ StringConvertPD.java
@@ -0,0 +1,17 @@
+public class StringConvertPD {
+    public static void main(String[] args) {
+        String name = "123";
+        // convert primtive data type
+        Integer i = Integer.parseInt(name);
+        System.out.println(i);
+        // wrapper convert into primitive data type
+        int b = i;
+        System.out.println(b);
+
+        // float :
+        String h = "8.9";
+        Float f = Float.parseFloat(h);
+        float g = f;
+        System.out.println(g);
+    }
+}
