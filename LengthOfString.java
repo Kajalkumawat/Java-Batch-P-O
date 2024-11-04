26dfc823401e9de87ef6d552c014684c823cd4fb
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-04 11:15:44 +0530

    LENGTH OF STRING IN JAVA PREDEFINE METHOD

---
+++ LengthOfString.java
@@ -0,0 +1,10 @@
+public class LengthOfString {
+    public static void main(String[] args) {
+        String name = "Regex";
+        System.out.println(name);
+        System.out.println(name.length());
+
+        String name1 = "hello";
+        System.out.println(name1);
+    }
+}
