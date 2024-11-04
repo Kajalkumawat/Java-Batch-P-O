a624680a1e3761aa19bc2b91b3307d4734906079
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-04 11:53:46 +0530

    CONSTANT AND DYNAMIC DIFFRENCE

---
+++ ConstantVSDynamicString.java
@@ -0,0 +1,8 @@
+public class ConstantVSDynamicString {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(s);
+        String name = new String("world");
+        System.out.println(name);
+    }
+}
