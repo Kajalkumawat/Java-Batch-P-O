5a7340e342af4a40189849e7ae20e7263c2bd5f1
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-05 11:14:48 +0530

    STRING BUFFER IN JAVA

---
+++ StringBufferBasic.java
@@ -0,0 +1,10 @@
+public class StringBufferBasic {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello world");
+        System.out.println(sb);
+        // string mutalbe (modify)
+        // append method :predefine
+        sb.append(" duniya");
+        System.out.println(sb);
+    }
+}
