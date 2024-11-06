ca67de075e80a7d275dd924325c54d6dda941821
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 11:40:31 +0530

    CHARAT METHOD IN STRING

---
+++ String/Stringmethod1.java
@@ -0,0 +1,9 @@
+public class Stringmethod1 {
+    public static void main(String[] args) {
+        String name = "kajal";
+        System.out.println(name.charAt(2));
+        char s = name.charAt(9);
+        System.out.println(s);
+        // j and string index out of bound
+    }
+}
