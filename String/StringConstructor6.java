3707f5f9b902c6965dd68e69c177bb4c2d05b76f
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 11:22:42 +0530

    STRING CONSTRUCTOR USING BYTE ARRAY TYPE IN JAVA

---
+++ String/StringConstructor6.java
@@ -0,0 +1,7 @@
+public class StringConstructor6 {
+    public static void main(String[] args) {
+        byte a[] = { 19, 99, 3, 4, 5 };
+        String sb = new String(a);
+        System.out.println(sb);
+    }
+}
