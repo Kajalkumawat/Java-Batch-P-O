5663b6c833835d31e4fe8880b8090772ba6f2387
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 11:13:14 +0530

    STRING CONSTRUCTOR 2

---
+++ String/StringConstructor2.java
@@ -0,0 +1,7 @@
+public class StringConstructor2 {
+    public static void main(String[] args) {
+        String name = "kajal kumawat";
+        String s = new String(name);
+        System.out.println(s);
+    }
+}
