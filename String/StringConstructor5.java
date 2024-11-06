793f3575dd5ed782931b7ebe3d339981a79efcff
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 11:19:32 +0530

    STRING CONSTRUCTOR USING CHARACTER ARRAY TYPE

---
+++ String/StringConstructor5.java
@@ -0,0 +1,7 @@
+public class StringConstructor5 {
+    public static void main(String[] args) {
+        char a[] = { 'a', 'o', 'p' };
+        String s = new String(a);
+        System.out.println(s);
+    }
+}
