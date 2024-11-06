1f3d93842133a924c78ece5c25f918b88ac1f008
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 10:55:40 +0530

    STRING BUILDER IN JAVA

---
+++ String/StringBuilderBasic.java
@@ -0,0 +1,7 @@
+public class StringBuilderBasic {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+        sb.append("word");
+        System.out.println(sb);
+    }
+}
