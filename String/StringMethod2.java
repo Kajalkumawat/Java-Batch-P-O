b0cc88ac156add8da292d3c17b5a2a639e3b278d
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-06 11:41:55 +0530

    CONCAT METHOD IN STRING IN JAVA

---
+++ String/StringMethod2.java
@@ -0,0 +1,7 @@
+public class StringMethod2 {
+    public static void main(String[] args) {
+        String s = new String("hello");
+        s = s.concat("world");
+        System.out.println(s);
+    }
+}
