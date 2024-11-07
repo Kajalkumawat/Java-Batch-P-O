d6d69417cdfe9cd540fac2dd8aacd44b5894e337
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-07 10:48:29 +0530

    REPLACE IN STRING

---
+++ String/StringMethod6.java
@@ -0,0 +1,6 @@
+public class StringMethod6 {
+    public static void main(String[] args) {
+        String s = "regex is a learning platform";
+        System.out.println(s.replace('e', 'o'));
+    }
+}
