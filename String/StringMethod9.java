08ca3bea079be0f40d63304b968ad7a2ea8c3f61
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-07 10:58:58 +0530

    TRIM METHOD IN STRING

---
+++ String/StringMethod9.java
@@ -0,0 +1,7 @@
+public class StringMethod9 {
+    public static void main(String[] args) {
+        String name = "regex is a learning platform  ";
+        System.out.println(name.trim() + "edtech");
+        System.out.println("software  " + name.trim());
+    }
+}
