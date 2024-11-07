f6cf2deff88c20966a0e20716eeb91c4f04822f0
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-07 10:44:34 +0530

    SUBSTRING METHOD IN JAVA PART II

---
+++ String/StringMethod5.java
@@ -0,0 +1,7 @@
+public class StringMethod5 {
+    public static void main(String[] args) {
+        
+        String name="regex is a learning platform";
+        System.out.println(name.substring(5,9));
+    }
+}
