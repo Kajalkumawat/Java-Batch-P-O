9904b73ba251503cab1a71d43bc040814b07018a
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-11 10:54:13 +0530

    reverse of words in string

---
+++ String/ReverseSplit.java
@@ -0,0 +1,9 @@
+public class ReverseSplit {
+    public static void main(String[] args) {
+        String s = "java is programming language";
+        String m[] = s.split(" ");
+        for (int i = m.length - 1; i >= 0; i--) {
+            System.out.print(m[i] + " ");
+        }
+    }
+}
