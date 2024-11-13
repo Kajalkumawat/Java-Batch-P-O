7dc015ca8af4c4bafbff3c3ac90c62acf4334e7e
kajalkumawatregexsoftware <kajal.kumawat@regexsoftware.com>
2024-11-13 12:26:28 +0530

    PRINT SINGLE DATA

---
+++ Collection/SingleData.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SingleData {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<>();
+        al.add("hello");// 0
+        al.add("hii");// 1
+        al.add("bye");// 2
+        al.add("how");// 3
+        System.out.println(al.get(2));
+        // want to size :used size method
+        System.out.println(al.size());
+    }
+}
