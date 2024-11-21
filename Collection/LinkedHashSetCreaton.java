01a624aaad130dfde990b9e525ce97b91399ea62
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-21 12:41:10 +0530

    LinkedHashSetCreaton

---
+++ Collection/LinkedHashSetCreaton.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class LinkedHashSetCreaton {
+    public static void main(String[] args) {
+        LinkedHashSet hs = new LinkedHashSet<>();
+        hs.add("hello");
+        hs.add(45);
+        System.out.println(hs);
+        System.out.println(hs.add("jii"));
+    }
+}
