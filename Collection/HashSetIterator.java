63114106fb31256da34382955ba69e8e566bae9c
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-21 12:16:09 +0530

    HASHSET PRINT THROUGH ITERATOR

---
+++ Collection/HashSetIterator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HashSetIterator {
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<>();
+        hs.add(67);
+        hs.add(12);
+        hs.add(97);
+        Iterator itr = hs.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
