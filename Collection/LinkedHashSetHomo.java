2ac922f8a1418e3878d2a087d30828278903ffbb
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 09:14:12 +0530

    LINKED HASH SET HOMOGENOUS DATA

---
+++ Collection/LinkedHashSetHomo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class LinkedHashSetHomo {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Apple");
+        lhs.add("hello");
+        System.out.println(lhs);
+    }
+}
