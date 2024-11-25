89dcce6dedfa8bd3a2de6c3964afc1c1541ed3c0
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-25 09:44:19 +0530

    TreeSetCreation

---
+++ Collection/TreeSetCreation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TreeSetCreation {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(6);
+        ts.add(1);
+        ts.add(9);
+        ts.add(3);
+        System.out.println(ts);
+    }
+}
