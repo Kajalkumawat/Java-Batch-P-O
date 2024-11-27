eb6c245907f3e85182a28feb1cf92144ff7ec903
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-27 09:48:26 +0530

    LINKED HASHMAP IN JAVA

---
+++ Collection/LinkedHashMapBasic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class LinkedHashMapBasic {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lhs = new LinkedHashMap();
+        lhs.put("one", 1);
+        lhs.put("two", 2);
+        System.out.println(lhs);
+        for(Map.Entry l:lhs.entrySet()){
+            System.out.println(l.getKey()+" "+l.getValue());
+        }
+    }
+}
