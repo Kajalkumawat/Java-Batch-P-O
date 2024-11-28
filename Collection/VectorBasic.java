c869a67de227f3c34b596055cdf1ab22768943ce
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-11-28 09:46:47 +0530

    VectorBasic

---
+++ Collection/VectorBasic.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+public class VectorBasic {
+    public static void main(String[] args) {
+        Vector v = new Vector<>();
+        v.addElement(78);
+        v.addElement(23);
+        v.addElement(56);
+        System.out.println(v);
+    }
+}
